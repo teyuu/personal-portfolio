6b67bc913b3ed65781c211d7e9ff146439d2a8b5
Matias Tellini <tellini.matias@gmail.com>
2022-09-30 20:42:06 -0300

    Update videogamesapp.jsx

--- pages/videogamesapp.jsx
+++ pages/videogamesapp.jsx
@@ -40,7 +40,7 @@ const videogamesapp = () => {
             <button className="px-8 py-2 mt-4 mr-8">Code</button>
           </a>
           <a
-            href="https://www.linkedin.com/feed/update/urn:li:activity:6964349437510856706/"
+            href="https://videogames-mt.vercel.app/home"
             target="_blank"
             rel="noreferrer"
           >
